db1780ae3209eaae70675cd97e3b517f237ef9e5
ariary <ariary9.2@hotmail.fr>
2022-02-18 14:43:05 +0100

    Flag stuff and filter

--- pkg/fuzz/config.go
+++ pkg/fuzz/config.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,6 +17,7 @@ type Config struct {
 	Shell            string
 	Timeout          int64
 	DisplayModes     []DisplayMode
+	Filters          []Filter
 	// OutputFilters    []display.OutputFilter
 	// TimeFilters      []TimeFilter
 	// CodeFilters      []CodeFilter
@@ -28,7 +30,7 @@ Fuzz command line execution and filter results
   -d, --delay        delay in ms between each thread launching. A thread execute the command. (default: 0)
   -k, --keyword      keyword use to determine which zone to fuzz (default: FUZZ)
   -s, --shell        shell to use for execution (default: /bin/bash)
-  -t, --timeout      command execution timeout in s. After reaching it the command is killed. (default: 30)
+  -to, --timeout      command execution timeout in s. After reaching it the command is killed. (default: 30)
   -h, --help         prints help information 
 `
 
@@ -49,49 +51,33 @@ func NewConfig() Config {
 	flag.StringVar(&config.Shell, "shell", "/bin/bash", "shell to use for execution")
 	flag.StringVar(&config.Shell, "s", "/bin/bash", "shell to use for execution")
 
-	// display mode: output
-	var outputCharacter, outputCharacterErr string
-	var outputMin, outputMax int
-	flag.StringVar(&outputCharacter, "oc", "", "display execution command number of characters in stdout. If value is provided, it displays only command with exactly the same value number of charcater in stdout")
-	flag.StringVar(&outputCharacterErr, "oce", "", "display execution command number of characters in stderr. If value is provided, it displays only command with exactly the same value number of charcater in stderr")
-	// output filter
-	flag.IntVar(&outputMin, "cmin", 0, "display command with a minimum number of characters in stdout")
-	flag.IntVar(&outputMax, "cmax", 0, "display command with a maximum number of characters in stdout")
-
-	// display mode: exit code
-	var exitCode string
-	var failure bool
-	flag.StringVar(&exitCode, "e", "", "display command exit code. If value is provided, it displays only command with the exit code")
-	// output filter
-	flag.BoolVar(&failure, "failure", false, "display only commands with exit code different of 0")
-
-	// display mode: time
-	var time int
-	var timeMin, timeMax int
-	flag.IntVar(&time, "ti", 0, "display command time execution. If value is provided, it displays only command with the same value time execution (+/-1s)")
-	// time filter
-	flag.IntVar(&timeMin, "tmin", 0, "display command with a minimum execution time")
-	flag.IntVar(&timeMax, "tmax", 0, "display command with a maximum execution time")
-
-	// var filter string
-	// flag.StringVar(&filter, "f", "output", "filter type to sort execution results")
-	// flag.StringVar(&filter, "filter", "output", "filter type to sort execution results")
-	// switch filter {
-	// case Output.String():
-	// 	config.FilterType = Output
-	// case Time.String():
-	// 	config.FilterType = Time
-	// case ReturnCode.String():
-	// 	config.FilterType = ReturnCode
-	// } //default: if unreadable keep output
-
 	//flag RoutineDelay
 	flag.Int64Var(&config.RoutineDelay, "d", 0, "delay in ms between each thread launching. A thread execute the command. (default: 0)")
 	flag.Int64Var(&config.RoutineDelay, "delay", 0, "delay in ms between each thread launching. A thread execute the command. (default: 0)")
 
 	//flag timeout
-	flag.Int64Var(&config.Timeout, "t", 30, "Command execution timeout in s. After reaching it the command is killed. (default: 30)")
+	flag.Int64Var(&config.Timeout, "to", 30, "Command execution timeout in s. After reaching it the command is killed. (default: 30)")
 	flag.Int64Var(&config.Timeout, "timeout", 30, "Command execution timeout in s. After reaching it the command is killed. (default: 30)")
+
+	// display mode
+	var stdoutDisplay bool
+	flag.BoolVar(&stdoutDisplay, "oc", false, "display command execution  number of characters in stdout.")
+	flag.BoolVar(&stdoutDisplay, "stdout-characters", false, "display execution command number of characters in stdout.")
+
+	var stderrDisplay bool
+	flag.BoolVar(&stderrDisplay, "ec", false, "display command execution  number of characters in stderr.")
+	flag.BoolVar(&stderrDisplay, "stderr-characters", false, "display execution command number of characters in stderr.")
+
+	var timeDisplay bool
+	flag.BoolVar(&timeDisplay, "t", false, "display command execution  time.")
+	flag.BoolVar(&timeDisplay, "time", false, "display command execution time.")
+
+	var codeDisplay bool
+	flag.BoolVar(&codeDisplay, "c", false, "display command execution exit code.")
+	flag.BoolVar(&codeDisplay, "code", false, "display command execution exit code.")
+
+	// filters
+	parseFilters(&config)
 
 	flag.Usage = func() { fmt.Print(usage) }
 	flag.Parse()
@@ -105,16 +91,7 @@ func NewConfig() Config {
 	}
 
 	// parse display mode
-	displays := parseDisplayMode()
-	if len(displays) > 0 {
-		config.DisplayModes = displays
-	} else {
-		stdoutDisplay := StdoutDisplay{}
-		config.DisplayModes = []DisplayMode{stdoutDisplay}
-	}
-
-	// parse filters
-	parseFilters(&config)
+	config.DisplayModes = parseDisplayMode(stdoutDisplay, stderrDisplay, timeDisplay, codeDisplay)
 
 	return config
 }
@@ -141,23 +118,134 @@ func (c *Config) CheckConfig() error {
 	return nil
 }
 
-//isFlagPassed: determine if flag is provided even with empty value
-func isFlagPassed(name string) bool {
-	found := false
-	flag.Visit(func(f *flag.Flag) {
-		if f.Name == name {
-			found = true
-		}
-	})
-	return found
-}
+// parseDisplayMode: parse display mode chosen with flag and return array of these display mode interface. If none, default is stdout characters display mode
+func parseDisplayMode(stdout bool, stderr bool, time bool, code bool) (modes []DisplayMode) {
+	if stdout {
+		modes = append(modes, StdoutDisplay{})
+	}
+	if stderr {
+		modes = append(modes, StderrDisplay{})
+	}
+	if time {
+		modes = append(modes, TimeDisplay{})
+	}
+	if code {
+		modes = append(modes, CodeDisplay{})
+	}
 
-// parseDisplayMode: parse all flags and determine the display modes chosen
-func parseDisplayMode() (modes []DisplayMode) {
+	//default, if none
+	if len(modes) < 0 {
+		stdoutDisplay := StdoutDisplay{}
+		modes = []DisplayMode{stdoutDisplay}
+	}
 	return modes
 }
 
-// parseFilters: parse all flags and determine the filters
-func parseFilters(cfg *Config) {
+// parseFilters: parse all flags and determine the filters, add them in config struct
+func parseFilters(config *Config) {
+	// stdout filters
+	flag.Func("omax", "filter to display only results with less than n characters", func(max string) error {
+		n, err := strconv.Atoi(max)
+		if err != nil {
+			return err
+		}
+		filter := StdoutMaxFilter{Max: n}
+		config.Filters = append(config.Filters, filter)
+		return nil
+	})
 
+	flag.Func("omin", "filter to display only results with more than n characters", func(min string) error {
+		n, err := strconv.Atoi(min)
+		if err != nil {
+			return err
+		}
+		filter := StdoutMinFilter{Min: n}
+		config.Filters = append(config.Filters, filter)
+		return nil
+	})
+
+	flag.Func("oeq", "filter to display only results with exactly n characters", func(eq string) error {
+		n, err := strconv.Atoi(eq)
+		if err != nil {
+			return err
+		}
+		filter := StdoutEqFilter{Eq: n}
+		config.Filters = append(config.Filters, filter)
+		return nil
+	})
+
+	// stderr filters
+	flag.Func("emax", "filter to display only results with less than n characters", func(max string) error {
+		n, err := strconv.Atoi(max)
+		if err != nil {
+			return err
+		}
+		filter := StderrMaxFilter{Max: n}
+		config.Filters = append(config.Filters, filter)
+		return nil
+	})
+
+	flag.Func("emin", "filter to display only results with more than n characters", func(min string) error {
+		n, err := strconv.Atoi(min)
+		if err != nil {
+			return err
+		}
+		filter := StderrMinFilter{Min: n}
+		config.Filters = append(config.Filters, filter)
+		return nil
+	})
+
+	flag.Func("eeq", "filter to display only results with exactly n characters", func(eq string) error {
+		n, err := strconv.Atoi(eq)
+		if err != nil {
+			return err
+		}
+		filter := StderrEqFilter{Eq: n}
+		config.Filters = append(config.Filters, filter)
+		return nil
+	})
+
+	// time filters
+	flag.Func("tmax", "filter to display only results with a time lesser than n seconds", func(max string) error {
+		n, err := strconv.Atoi(max)
+		if err != nil {
+			return err
+		}
+		filter := TimeMaxFilter{Max: n}
+		config.Filters = append(config.Filters, filter)
+		return nil
+	})
+
+	flag.Func("tmin", "filter to display only results with a time greater than n seconds", func(min string) error {
+		n, err := strconv.Atoi(min)
+		if err != nil {
+			return err
+		}
+		filter := TimeMinFilter{Min: n}
+		config.Filters = append(config.Filters, filter)
+		return nil
+	})
+
+	flag.Func("teq", "filter to  display only results with a time equal to n seconds", func(eq string) error {
+		n, err := strconv.Atoi(eq)
+		if err != nil {
+			return err
+		}
+		filter := TimeEqFilter{Eq: n}
+		config.Filters = append(config.Filters, filter)
+		return nil
+	})
+
+	// code filters
+	var success, failure bool
+	flag.BoolVar(&success, "success", false, "filter to display only command with exit code 0.")
+	flag.BoolVar(&failure, "failure", false, "filter to display only command with a non-zero exit .")
+	if success {
+		filter := CodeSuccessFilter{Zero: true}
+		config.Filters = append(config.Filters, filter)
+	}
+	if failure {
+		filter := CodeSuccessFilter{Zero: false}
+		config.Filters = append(config.Filters, filter)
+	}
 }
--- pkg/fuzz/filter.go
+++ pkg/fuzz/filter.go
@@ -1,36 +1,101 @@
 package fuzz
 
-// OutputFilter: output filter
-type OutputFilter int64
+// DisplayMode: interface use to determine field to display in cfuzz errput
+type Filter interface {
+	IsOk(result ExecResult) bool
+}
 
-const (
-	CharacterExact OutputFilter = iota
-	CharacterMax
-	CharacterMin
-)
+// StdoutMaxFilter: Filter that accept only result with less characters than a specific number
+type StdoutMaxFilter struct {
+	Max int
+}
 
-// StderrFilter: output filter
-type StderrFilter int64
+// IsOk: return true if the lenght of stdout output is smaller or equal than max
+func (maxFilter StdoutMaxFilter) IsOk(result ExecResult) bool {
+	return len(result.Stdout) <= maxFilter.Max
+}
 
-const (
-	StderrCharacterExact OutputFilter = iota
-	StderrCharacterMax
-	StderrCharacterMin
-)
+// StdoutMaxFilter: Filter that accept only result with more characters than a specific number
+type StdoutMinFilter struct {
+	Min int
+}
 
-// TimeFilter: type of field to display to cfuzz result
-type TimeFilter int64
+// IsOk: return true if the lenght of stdout output is greater or equal than min
+func (filter StdoutMinFilter) IsOk(result ExecResult) bool {
+	return len(result.Stdout) >= filter.Min
+}
 
-const (
-	TimeExact OutputFilter = iota
-	TimeMax
-	TimeMin
-)
+type StdoutEqFilter struct {
+	Eq int
+}
 
-// CodeFilter: type of field to display to cfuzz result
-type CodeFilter int64
+func (filter StdoutEqFilter) IsOk(result ExecResult) bool {
+	return len(result.Stdout) == filter.Eq
+}
 
-const (
-	CodeExact = iota
-	CodeFalure
-)
+// StderrMaxFilter: Filter that accept only result with less characters than a specific number
+type StderrMaxFilter struct {
+	Max int
+}
+
+// IsOk: return true if the lenght of stderr errput is smaller or equal than max
+func (maxFilter StderrMaxFilter) IsOk(result ExecResult) bool {
+	return len(result.Stderr) <= maxFilter.Max
+}
+
+// StderrMaxFilter: Filter that accept only result with more characters than a specific number
+type StderrMinFilter struct {
+	Min int
+}
+
+// IsOk: return true if the lenght of stderr errput is greater or equal than min
+func (filter StderrMinFilter) IsOk(result ExecResult) bool {
+	return len(result.Stderr) >= filter.Min
+}
+
+type StderrEqFilter struct {
+	Eq int
+}
+
+func (filter StderrEqFilter) IsOk(result ExecResult) bool {
+	return len(result.Stderr) == filter.Eq
+}
+
+type TimeMaxFilter struct {
+	Max int
+}
+
+func (maxFilter TimeMaxFilter) IsOk(result ExecResult) bool {
+	return int(result.Time.Seconds()) <= maxFilter.Max
+}
+
+type TimeMinFilter struct {
+	Min int
+}
+
+func (filter TimeMinFilter) IsOk(result ExecResult) bool {
+	return int(result.Time.Seconds()) >= filter.Min
+}
+
+type TimeEqFilter struct {
+	Eq int
+}
+
+func (filter TimeEqFilter) IsOk(result ExecResult) bool {
+	return int(result.Time.Seconds()) == filter.Eq
+}
+
+// CodeSuccessFilter: filter wether result regarding the exit code
+type CodeSuccessFilter struct {
+	Zero bool
+}
+
+// IsOk: return true if the exit code is 0 and the filter is parametrize with Zero set at true. return true if the exit code is != 0 and the filter is parametrize with Zero set at false.
+// return false otherwise
+func (filter CodeSuccessFilter) IsOk(result ExecResult) bool {
+	if filter.Zero { // --success
+		return result.Code == "0"
+	} else {
+		return result.Code != "0"
+	}
+}
